316b0443c697d2b057658191fd4bcf0260f55a91
Harold Fich <haroldfich@gmail.com>
2022-02-10 14:10:46 -0500

    client/backend connection - axios req for state

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
 app.use("/api/v1/transactions", transactions);
 
 app.listen(
